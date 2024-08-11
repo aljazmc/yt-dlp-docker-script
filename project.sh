2b88cf866cc3b72f86cfc9625fef80ac97fc450b
aljazmc <mlinaric.aljaz@gmail.com>
2024-08-11 14:44:54 +0200

    fix: ffmpeg

--- project.sh
+++ project.sh
@@ -37,19 +37,20 @@ mkdir -p .local .cache/pip .cache/yt-dlp/youtube-nsig
   if [[ ! -f Dockerfile ]]; then
     touch Dockerfile && \
     cat <<EOF> Dockerfile
-  FROM python:latest
+FROM debian:bookworm
 
-  ENV DEBIAN_FRONTEND=noninteractive
-  ENV USER=$USER
+ENV LANG C.UTF-8
 
-  RUN dpkg apt-get update && \
-    apt-get install -y \
-    ffmpeg \
-    libav-tools
-
-  ENV LC_ALL $PROJECT_LANG
-  ENV LANG $PROJECT_LANG
-  ENV LANGUAGE $PROJECT_LANG
+# runtime dependencies
+RUN set -eux && \
+	apt-get update && apt-get install -y --no-install-recommends \
+      ffmpeg \
+      libbluetooth-dev \
+      tk-dev \
+      python3.11 \
+      python3-pip \
+      sudo \
+      uuid-dev && rm -rf /var/lib/apt/lists/*
 
   RUN groupadd -g $PROJECT_GID -r $USER
   RUN useradd -u $PROJECT_UID -g $PROJECT_GID --create-home -r $USER
@@ -64,6 +65,8 @@ mkdir -p .local .cache/pip .cache/yt-dlp/youtube-nsig
   USER $USER
 
   WORKDIR /home/$USER
+
+CMD ["python3"]
 EOF
 fi
 
@@ -72,7 +75,7 @@ if [[ ! -f docker-compose.yml ]]; then
 services:
   yt-dlp:
     build: .
-    image: python:latest
+    image: yt-dlp
     user: $PROJECT_UID:$PROJECT_GID
     working_dir: /home/$USER
     volumes:
@@ -84,7 +87,7 @@ services:
 EOF
 fi
 
-docker compose run yt-dlp pip3 install --user yt-dlp
+docker compose run yt-dlp pip3 install --break-system-packages --user yt-dlp
 docker compose run yt-dlp python3 .local/bin/yt-dlp --version
 
 }
