b3ad911ce67bd42c1032c6c98dcbb50db0bba7b4
aljazmc <mlinaric.aljaz@gmail.com>
2025-02-28 08:11:01 +0200

    chore: make user configuration line in docker-compose.yml OS dependent

--- project.sh
+++ project.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-## Check if OS is Linux or quit
-
-if [[ "$OSTYPE" != "linux-gnu"* ]]; then
-    echo "Script runs only on GNU/Linux OS. Exiting..."
-    exit
-fi
-
 ## Check if docker compose is installed or quit
 
 if [[ ! -x "$(command -v compose version)" ]]; then
@@ -77,7 +70,6 @@ if [[ ! -f docker-compose.yml ]]; then
 services:
   yt-dlp:
     build: .
-    user: $PROJECT_UID:$PROJECT_GID
     working_dir: /home/$USER
     volumes:
       - .:/home/$USER
@@ -86,6 +78,12 @@ services:
     environment:
       PATH:     "/.local/bin:/home/$USER/.local/bin:\$PATH"
 EOF
+
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Adding user configuration line to docker-compose.yml for GNU/Linux users."
+    sed -i "3 a \ \ \ \ user\:\ $PROJECT_UID\:$PROJECT_GID" docker-compose.yml
+  fi
+
 fi
 
 docker compose run --rm yt-dlp pip3 install --break-system-packages --user yt-dlp
